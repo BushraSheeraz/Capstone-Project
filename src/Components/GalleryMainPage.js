fc73f9228958190e339eb2efef93c44a951872c3
SUMERA222 <sumera.hassan85@gmail.com>
2021-10-07 11:54:42 -0700

    Created & updated Page 2(Gallery Main Page)
    Created & updated Page 2 and its contents.

--- src/Components/GalleryMainPage.js
+++ src/Components/GalleryMainPage.js
@@ -1,10 +1,17 @@
 import Card from 'react-bootstrap/Card';
-
-import image1 from '../Images/gallery3.jpg';
-import image2 from '../Images/gallery2.jpg';
-import image3 from '../Images/gallery11.jpg';
+import NavBar from './NavBar';
+import image1 from '../Images/DM-Std1.jpg';
+import image2 from '../Images/DM-Std2.jpg';
+import image3 from '../Images/mentor1.jpg';
+import image4 from '../Images/impact2.jpg';
+import image5 from '../Images/DigM5.jpg';
+import image6 from '../Images/impact11.jpg';
 import CardGroup from 'react-bootstrap/CardGroup';
 
+<div>
+<GalleryPageContext/>
+                    
+</div>
 function GalleryMainPage() {
     return (
         <>
@@ -28,7 +35,31 @@ function GalleryMainPage() {
                 <Card>
                     <Card.Img variant="top" src={image3} />
                     <Card.Body>
-                        <Card.Title>Circle in Gilgit-Baltistan</Card.Title>
+                        <Card.Title>Circle's Mentor Meetup-2021</Card.Title>
+                        
+                    </Card.Body>
+                   
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={image4} />
+                    <Card.Body>
+                        <Card.Title>Circle Arranges Session on Free-Lancing</Card.Title>
+                        
+                    </Card.Body>
+                   
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={image5} />
+                    <Card.Body>
+                        <Card.Title>Tech Karo initiative to Digital Marketing Course</Card.Title>
+                        
+                    </Card.Body>
+                   
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={image6} />
+                    <Card.Body>
+                        <Card.Title>Always ready to enlighten people of Pakistan</Card.Title>
                         
                     </Card.Body>
                    
---
+++ src/Components/Gallerypagecontext.js
@@ -0,0 +1,25 @@
+
+function GalleryPageContext() {
+    return (
+        <>
+            <div className="context of gallery">
+                <h1>
+                    How circle Impacts Our Society
+                </h1>
+                <p>	Circle is the non-profit tech organization founded by Sadaffe Abid in 2014. 
+                    Tech Karo is its training program; for students from underprivileged areas.
+                    Our Mission is Women Economic Empowerment and Inclusion in terms of technology. 
+                    Our Goal is to improve women to develop their entrepreneurial and leadership capacity.
+                    We focus on training women for digital skills such as coding, Graphic designing and 
+                    Digital marketing to increase their livelihoods.To close the digital gender divide, 
+                    Tech Karo was initiated in Karachi on 22nd February 2018. This training program run by CIRCLE,
+                    where students are provided with tech education to enhance their economic opportunities, 
+                    tech mentors to provide inspiration to change mindsets, and special training for women to overcome 
+                    their constraints and embrace the future as tech professionals.
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default GalleryPageContext;
