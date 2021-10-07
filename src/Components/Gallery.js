f24679eb2a65b349b1893d4e4e9ff87d70803465
BushraSheeraz <bushragemini14@gmail.com>
2021-10-08 01:09:48 +0500

    Updating Gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -3,37 +3,31 @@ import Button from 'react-bootstrap/Button';
 import image1 from '../Images/gallery3.jpg';
 import image2 from '../Images/gallery2.jpg';
 import image3 from '../Images/gallery11.jpg';
-import CardGroup from 'react-bootstrap/CardGroup';
+// import CardGroup from 'react-bootstrap/CardGroup';
 
 function Gallery() {
     return (
         <>
-            <CardGroup>
-                <Card>
-                    <Card.Img variant="top" src={image1} />
+            <div className="gallery" style={{display:'flex'}}>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={image1}/>
                     <Card.Body>
-                    <Card.Title>Our Alumni Stories</Card.Title>
-                    <Button variant="dark">Read more</Button>
-                    </Card.Body>                                   
+                        {/* <Card.Title>Our Alumini</Card.Title> */}
+                    </Card.Body>
                 </Card>
-                <Card>
-                    <Card.Img variant="top" src={image2} />
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={image2}/>
                     <Card.Body>
-                        <Card.Title>
-                            Circle introduces She Loves Tech - Startup</Card.Title>
-                            <Button variant="dark">Read more</Button>
-                        </Card.Body>
-                   
+                        {/* <Card.Title>She Loves Tech</Card.Title> */}
+                    </Card.Body>
                 </Card>
-                <Card>
+                <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src={image3} />
                     <Card.Body>
-                        <Card.Title>Circle in Gilgit-Baltistan</Card.Title>
-                        <Button variant="dark">Read more</Button>
+                        {/* <Card.Title>Tech Karo in Gilgit</Card.Title> */}
                     </Card.Body>
-                    
                 </Card>
-            </CardGroup>
+            </div>
         </>
     )
 }
