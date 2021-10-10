faf4c6817d39d208934cc2e0621bc9027b03b6c5
Bushra Sheeraz <bushragemini14@gmail.com>
2021-10-11 01:11:02 +0500

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -13,30 +13,30 @@ import TechKaroPage from "./Pages/TechKaroPage";
 function NavBar() {
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" style={{ marginBottom: '20px', boxShadow: '12px 10px #f1eeee', backgroundColor: 'white' }}>
-                <Container >
-                    <Navbar.Brand as={Link} to="/home">
-                        <img
-                            src={logo}
-                            width="60px"
-                            className="d-inline-block align-top"
-                            alt="Circle women logo"
-                        />
-                    </Navbar.Brand>
-                </Container>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Container>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/techkaropage">About us</Nav.Link>
-                            <Nav.Link as={Link} to="/registration">Registration</Nav.Link>
-                            <Nav.Link as={Link} to="/gallerymainpage">Gallery</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-
+                <Navbar collapseOnSelect expand="lg" style={{ marginBottom: '20px', boxShadow: '12px 10px #f1eeee', backgroundColor:'white' }}>
+                    <Container >
+                        <Navbar.Brand href="/">
+                            <img
+                                src={logo}
+                                width="60px"
+                                className="d-inline-block align-top"
+                                alt="Circle women logo"
+                            />
+                        </Navbar.Brand>
+                    </Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Container>
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                                <Nav.Link as={Link} to="/techkaropage">About us</Nav.Link>
+                                <Nav.Link as={Link} to="/registration">Registration</Nav.Link>
+                                <Nav.Link as={Link} to="/gallerymainpage">Gallery</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+               
             {/* <Home /> */}
 
 
